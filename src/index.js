ca0d50bc0c459db9c417539098c12f54a37b8285
Praveen C <praveen.chandramohan@tcs.com>
2023-02-09 21:08:58 +0530

    react-learning

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider, combineReducer, applyMiddleWare } from 'react-redux';
+//import {thunk} from 'redux-thunk';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// const rootReducer = combineReducer({
+//   pr: productReducer,
+//   lr: loginReducer
+// })
+
+// const store = createStore(rootReducer, applyMiddleWare(thunk));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
